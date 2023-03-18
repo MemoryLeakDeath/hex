7c31afa253963b1a326e9e450afdc0893e4c5ff2
MemoryLeakDeath <memoryleakdeath@gmail.com>
2023-03-18 19:36:34 -0400

    Got a simple web app working for the moment with Thymeleaf and Spring MVC.

--- src/main/java/tv/memoryleakdeath/hex/config/HexWebConfig.java
+++ src/main/java/tv/memoryleakdeath/hex/config/HexWebConfig.java
@@ -1,5 +1,8 @@
 package tv.memoryleakdeath.hex.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,34 +15,38 @@ import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan("tv.memoryleakdeath.hex.controller")
-public class HexWebConfig implements WebMvcConfigurer {
+public class HexWebConfig implements WebMvcConfigurer, ApplicationContextAware {
+	private ApplicationContext ac;
 
-	public class WebMvcConfig implements WebMvcConfigurer {
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setApplicationContext(ac);
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML");
+		return templateResolver;
+	}
 
-		@Bean
-		public SpringResourceTemplateResolver templateResolver() {
-			SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-			templateResolver.setPrefix("/WEB-INF/views/");
-			templateResolver.setSuffix(".html");
-			templateResolver.setTemplateMode("HTML");
-			return templateResolver;
-		}
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		return templateEngine;
+	}
 
-		@Bean
-		public SpringTemplateEngine templateEngine() {
-			SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-			templateEngine.setTemplateResolver(templateResolver());
-			return templateEngine;
-		}
+	@Bean
+	public ThymeleafViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setOrder(1);
+		viewResolver.setViewNames(new String[] { "*.html" });
+		return viewResolver;
+	}
 
-		@Bean
-		public ThymeleafViewResolver viewResolver() {
-			ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-			viewResolver.setTemplateEngine(templateEngine());
-			viewResolver.setOrder(1);
-			viewResolver.setViewNames(new String[] { "*.html" });
-			return viewResolver;
-		}
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.ac = applicationContext;
 	}
 
 }
--- src/main/java/tv/memoryleakdeath/hex/controller/HomeController.java
+++ src/main/java/tv/memoryleakdeath/hex/controller/HomeController.java
@@ -3,15 +3,17 @@ package tv.memoryleakdeath.hex.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
 	@GetMapping("/")
 	public String view(HttpServletRequest request, Model model) {
 		model.addAttribute("helloMsg", "Hello World!");
-		return "index";
+		return "index.html";
 	}
 }
