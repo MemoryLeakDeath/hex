a0610f9f23b7be68498e629b168b32af1421ec17
MemoryLeakDeath <memoryleakdeath@gmail.com>
2023-07-10 19:43:36 -0400

    - Fixed and expanded tests.

--- src/test/java/tv/memoryleakdeath/hex/fontend/controller/AbstractControllerTest.java
+++ src/test/java/tv/memoryleakdeath/hex/fontend/controller/AbstractControllerTest.java
@@ -1,16 +1,29 @@
 package tv.memoryleakdeath.hex.fontend.controller;
 
+import java.util.Arrays;
+import java.util.UUID;
+
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import net.datafaker.Faker;
+import tv.memoryleakdeath.hex.backend.security.HexUserDetailsService;
+import tv.memoryleakdeath.hex.common.pojo.HexUser;
 import tv.memoryleakdeath.hex.config.HexApplicationConfig;
 import tv.memoryleakdeath.hex.config.HexSecurity;
 import tv.memoryleakdeath.hex.config.HexWebConfig;
@@ -27,7 +40,7 @@ public abstract class AbstractControllerTest {
 
     @BeforeEach
     public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(SecurityMockMvcConfigurers.springSecurity()).build();
     }
 
     public MockMvc getMockMvc() {
@@ -36,6 +49,22 @@ public abstract class AbstractControllerTest {
 
     @Configuration
     abstract static class Config {
+
+        @Bean
+        @Primary
+        public UserDetailsService getMockUserDetailsService() {
+            return Mockito.mock(HexUserDetailsService.class);
+        }
+    }
+
+    public HexUser dummyNormalUser() {
+        Faker faker = new Faker();
+        HexUser normalUser = new HexUser("user", "password12", true, true, true, true, Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
+        normalUser.setDisplayName(faker.greekPhilosopher().name());
+        normalUser.setId(UUID.randomUUID().toString());
+        String emailAddress = faker.appliance().brand();
+        normalUser.setGravatarId(DigestUtils.md5Hex(emailAddress));
+        return normalUser;
     }
 
 }
--- src/test/java/tv/memoryleakdeath/hex/fontend/controller/HomeControllerTest.java
+++ src/test/java/tv/memoryleakdeath/hex/fontend/controller/HomeControllerTest.java
@@ -1,7 +1,11 @@
 package tv.memoryleakdeath.hex.fontend.controller;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -12,9 +16,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.ContextConfiguration;
 
 import tv.memoryleakdeath.hex.backend.dao.TestDao;
+import tv.memoryleakdeath.hex.common.pojo.HexUser;
 import tv.memoryleakdeath.hex.frontend.controller.BaseFrontendController;
 
 @ContextConfiguration(classes = { HomeControllerTest.Config.class })
@@ -33,11 +40,14 @@ public class HomeControllerTest extends AbstractControllerTest {
     @Autowired
     private TestDao mockTestDao;
 
+    @Autowired
+    private UserDetailsService mockUserDetailsService;
+
     @Test
     public void testTestDatabase() throws Exception {
         when(mockTestDao.testDatabase()).thenReturn(false);
         
-        getMockMvc().perform(get("/test"))
+        getMockMvc().perform(get("/test").with(csrf()))
         .andExpect(status().isOk())
         .andExpect(view().name("layout/main"))
         .andExpect(model().attribute("testResults", false))
@@ -45,11 +55,27 @@ public class HomeControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    public void testHomepage() throws Exception {
-        getMockMvc().perform(get("/"))
+    @WithAnonymousUser
+    public void testAnonymousHomepage() throws Exception {
+        getMockMvc().perform(get("/").with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("layout/main"))
                 .andExpect(model().attribute(BaseFrontendController.PAGE_TITLE, "Home"))
-                .andExpect(model().attribute("view", "index"));
+                .andExpect(model().attribute("view", "index"))
+                .andExpect(content().string(containsString("topNavRegistrationButton")))
+                .andExpect(content().string(containsString("topNavLoginButton")));
+    }
+
+    @Test
+    public void testUserHomepage() throws Exception {
+        HexUser user = dummyNormalUser();
+        getMockMvc().perform(get("/").with(csrf()).with(user(user)))
+                .andExpect(status().isOk())
+                .andExpect(view().name("layout/main"))
+                .andExpect(model().attribute(BaseFrontendController.PAGE_TITLE, "Home"))
+                .andExpect(model().attribute("view", "index"))
+                .andExpect(content().string(containsString("userAvatar")))
+                .andExpect(content().string(containsString("userDisplayName")))
+                .andExpect(content().string(containsString(user.getDisplayName())));
     }
 }
