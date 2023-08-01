428a7a4e022e485ddd16254295e1829124541d74
MemoryLeakDeath <memoryleakdeath@gmail.com>
2023-07-31 21:19:35 -0400

    - forgot to make database name/database user a variable replacement in the database init script for Docker.

--- src/main/resources_filtered/hex-db-init.sh
+++ src/main/resources_filtered/hex-db-init.sh
@@ -2,9 +2,9 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER hextest WITH PASSWORD '${dbpassword}';
-	CREATE DATABASE hex WITH OWNER = hextest ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1 IS_TEMPLATE = False;
-	GRANT ALL PRIVILEGES ON DATABASE hex TO hextest;
+	CREATE USER ${dbuser} WITH PASSWORD '${dbpassword}';
+	CREATE DATABASE ${dbname} WITH OWNER = ${dbuser} ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1 IS_TEMPLATE = False;
+	GRANT ALL PRIVILEGES ON DATABASE ${dbname} TO ${dbuser};
 	CREATE USER hextester WITH PASSWORD '${dbpassword}';
 	CREATE DATABASE hextest WITH OWNER = hextester ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1 IS_TEMPLATE = False;
 	GRANT ALL PRIVILEGES ON DATABASE hextest TO hextester;
